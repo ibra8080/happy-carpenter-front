4138d7f1202dd1164d5a917a23fa0e443e8c6922
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-08-10 16:51:25 +0000

    Add fontawsom, googlefonts links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import logo from '../asset/logo.png';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand as={Link} to="/">Happy Carpenter</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">
+          <img src={logo} alt="logo" height="45" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
