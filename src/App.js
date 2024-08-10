2b69fc386010a0ec262b7c741fca5e336e6503bf
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-08-10 16:16:39 +0000

    Transfer components form old repo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import Button from 'react-bootstrap/Button';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './api/axiosDefaults';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Button variant="primary">Primary</Button>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
