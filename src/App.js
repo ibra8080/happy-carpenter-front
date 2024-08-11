5bfb61166be09759659444accc6627b412bce81b
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-08-11 10:06:42 +0000

    Update axiosDefults,SignUpform

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './api/axiosDefaults';
-import NavBar from './components/NavBar';
-import Home from './components/Home';
-import LoginForm from './components/LoginForm';
-import RegisterForm from './components/RegisterForm';
-import styles from './App.module.css';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Routes } from "react-router-dom";
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
     <Router>
       <div className={styles.App}>
         <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/register" element={<RegisterForm />} />
-        </Routes>
+        <Container className={styles.Main}>
+          <Routes>
+            <Route path="/" element={<h1>Home page</h1>} />
+            <Route path="/signin" element={<h1>Sign in</h1>} />
+            <Route path="/signup" element={<SignUpForm />} />
+            <Route path="*" element={<p>Page not found!</p>} />
+          </Routes>
+        </Container>
       </div>
     </Router>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,47 @@
-import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import logo from '../asset/logo.png';
-import { Link } from 'react-router-dom';
-import styles from '../styles/NavBar.module.css';
+
+
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar className={styles.NavBar} bg="light" expand="lg">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand as={Link} to="/">
-          <img src={logo} alt="logo" height="45" />
-        </Navbar.Brand>
+        <NavLink to="/" className={styles.NavLink}>
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="45" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/login">Login</Nav.Link>
-            <Nav.Link as={Link} to="/register">Register</Nav.Link>
+          <Nav className="ml-auto text-left">
+            <NavLink 
+              to="/" 
+              className={({ isActive }) => 
+                `${styles.NavLink} ${isActive ? styles.Active : ""}`
+              }
+            >
+              <i className="fas fa-home"></i>Home
+            </NavLink>
+            <NavLink 
+              to="/signin" 
+              className={({ isActive }) => 
+                `${styles.NavLink} ${isActive ? styles.Active : ""}`
+              }
+            >
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </NavLink>
+            <NavLink 
+              to="/signup" 
+              className={({ isActive }) => 
+                `${styles.NavLink} ${isActive ? styles.Active : ""}`
+              }
+            >
+              <i className="fas fa-user-plus"></i>Sign up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
