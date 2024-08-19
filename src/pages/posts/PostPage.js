76457b5457d23aacd5910f3d84a0b0f25bb91f2e
Ibrahim Abusiaf <ibrahimabusaif.designer@gmail.com>
2024-08-19 19:18:02 +0000

    Update Postpage

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container, Badge } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "../../components/Post";
@@ -35,6 +35,12 @@ function PostPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <Post {...post.results[0]} setPosts={setPost} postPage />
+        <Container>
+          <Badge variant="secondary">Filter: {post.results[0]?.image_filter}</Badge>
+          {post.results[0]?.categories.map((category, index) => (
+            <Badge key={index} variant="info" className="ml-2">{category}</Badge>
+          ))}
+        </Container>
         <Container className="mt-3">
           {currentUser ? (
             <CommentCreateForm
